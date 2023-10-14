bd495ac87bced7c954a5225d2cad40d0eee869b7
Karthik Krishna <karthik4kittu@gmail.com>
2023-10-14 16:27:35 -0400

    Added Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
+import { Route } from "react-router";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route index element={""} />
+        <Route path="posts/:id" element={""} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
