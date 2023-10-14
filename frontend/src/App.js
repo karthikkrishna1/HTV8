d3c5dbe56c3d19733800e1baf624025562f3382a
Divyanshuoft <divyansh.kachchhava@mail.utoronto.ca>
2023-10-14 02:58:37 -0400

    added dash board

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,106 @@
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header className="App-header">
+      <a href="page.js">
+
+        <div className="post">
+        <div className="post-header">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/2/2f/No-photo-m.png"
+              alt="User Profile"
+              className="user-profile-image"
+            />
+            <p className="user-name">Anonymous Owl</p>
+          </div>
+        <p className="post-name">A man stabbed on ttc</p>
+          <img src="https://torontolife.com/wp-content/uploads/2023/07/TTC-main-scaled.jpg" alt="Post" className="post-image" />
+          <div className="image-overlay">
+              <p className="overlay-text">Learn More</p>
+            </div>
+        </div>
+        </a>
+        <div className="post">ß
+        <div className="post-header">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/2/2f/No-photo-m.png"
+              alt="User Profile"
+              className="user-profile-image"
+            />
+            <p className="user-name">Anonymous Poet</p>
+          </div>
+        <p className="post-name">Fire in Waterloo</p>
+          <img src="https://www.ctvnews.ca/polopoly_fs/1.908681.1344527796!/httpImage/image.jpeg_gen/derivatives/landscape_620/image.jpeg" alt="Post" className="post-image" />
+          <div className="image-overlay">
+              <p className="overlay-text">Learn More</p>
+            </div>
+        </div>
+        <div className="post">
+        <div className="post-header">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/2/2f/No-photo-m.png"
+              alt="User Profile"
+              className="user-profile-image"
+            />
+            <p className="user-name">Anonymous Policeman</p>
+          </div>
+        <p className="post-name">Angry mob killed their leader</p>
+          <img src="https://i.cbc.ca/1.5731830.1600627172!/fileImage/httpImage/image.JPG_gen/derivatives/16x9_780/saulnierville-sunday.JPG" alt="Post" className="post-image" />
+          <div className="image-overlay">
+              <p className="overlay-text">Learn More</p>
+            </div>
+        </div>
+        <div className="post">
+        <div className="post-header">
+            <img
+              src="https://c.files.bbci.co.uk/C356/production/_113760005_mediaitem113760004.jpg"
+              alt="User Profile"
+              className="user-profile-image"
+            />
+            <p className="user-name">Anonymous Ghost</p>
+          </div>
+        <p className="post-name">Racist white lady says she isn't racist</p>
+          <img src="https://c.files.bbci.co.uk/C356/production/_113760005_mediaitem113760004.jpg" alt="Post" className="post-image" />
+        
+          <div className="image-overlay">
+              <p className="overlay-text">Learn More</p>
+            </div></div>
+        <div className="post">
+        <div className="post-header">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/2/2f/No-photo-m.png"
+              alt="User Profile"
+              className="user-profile-image"
+            />
+            <p className="user-name">Anonymous King</p>
+          </div>
+        <p className="post-name">Rift between Right wing and Left wing</p>
+          <img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/C14E/production/_108968494_mediaitem108968493.jpg" alt="Post" className="post-image" />
+          <div className="image-overlay">
+              <p className="overlay-text">Learn More</p>
+            </div>
+        </div>
+        <div className="post">
+        <div className="post-header">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/2/2f/No-photo-m.png"
+              alt="User Profile"
+              className="user-profile-image"
+            />
+            <p className="user-name">Anonymous Pirate</p>
+          </div>
+        <p className="post-name">Pizza man sexually assulted black pregnant lady</p>
+          <img src="https://www.ctvnews.ca/content/dam/ctvnews/en/images/2023/10/9/suspect-1-6594512-1697028290397.jpeg" alt="Post" className="post-image" />
+          <div className="image-overlay">
+              <p className="overlay-text">Learn More</p>
+            </div>
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default App;
